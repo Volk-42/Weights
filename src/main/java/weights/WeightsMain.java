dbbf0defdcf8ca5aacf5794a985bdf4af2e7739f
Volkisch <jetpaine@gmail.com>
2020-01-13 19:44:00 -0500

    added boring but big function

--- src/main/java/weights/WeightsMain.java
+++ src/main/java/weights/WeightsMain.java
@@ -6,8 +6,20 @@ public class WeightsMain {
     public static void main(String[] args) {
         
         int currentWeek = 2;
-        double current1RM = 200;
+        double squat1RM = 240;
+        double press1RM = 95;
+        double deadLift1RM = 215;
+        double bench1RM = 155;
+        double bbbPercentage = .45;
         int currentBar = 35;
-        System.out.println(new Workout(currentWeek, current1RM, currentBar).getWorkout());
+        
+        System.out.println("Squat \n" + new Workout(currentWeek, squat1RM, 
+                bbbPercentage, currentBar).getWorkout());
+        System.out.println("Press \n" + new Workout(currentWeek, press1RM, 
+                bbbPercentage, currentBar).getWorkout());
+        System.out.println("Deadlift \n" + new Workout(currentWeek, deadLift1RM, 
+                bbbPercentage, currentBar).getWorkout());
+        System.out.println("Bench \n" + new Workout(currentWeek, bench1RM, 
+                bbbPercentage, currentBar).getWorkout());
     }
 }
--- src/main/java/weights/Workout.java
+++ src/main/java/weights/Workout.java
@@ -5,6 +5,9 @@ public class Workout {
     
     private int week;
     private double oneRepMax;
+    private double bbbPercentage;
+    private double bbbWeight;
+    private String bbbPlates = "";
     private int barWeight;
     private String[] reps = {"5","5","5+",
                             "3","3","3+",
@@ -19,11 +22,13 @@ public class Workout {
     private double[] platesOwned = {45,25,25,10,10,5,2.5};
     private int offSet;
     
-    public Workout (int week, double oneRepMax, int barWeight) { 
+    public Workout (int week, double oneRepMax, 
+                        double bbbPercentage, int barWeight) { 
         this.week = week;
         this.oneRepMax = oneRepMax;
+        this.bbbPercentage = bbbPercentage;
         this.barWeight = barWeight;
-        int offSet = (week-1)*3;
+        this.offSet = (week-1)*3;
         calcWeights();
         calcPlates();
     }
@@ -33,18 +38,29 @@ public class Workout {
             int i2 = i+offSet;
             weight[i]=oneRepMax*weightPercentage[i2];
             weight[i]=5*(Math.round(weight[i]/5));
+            bbbWeight=oneRepMax*bbbPercentage;
+            bbbWeight=5*(Math.round(bbbWeight/5));
         }
     }
     
     private void calcPlates() {
         double[] weightHolder = new double[3];
+        double bbbHolder = bbbWeight-barWeight;
         for (int i=0; i < 3; i++) {
+            
             weightHolder[i]=weight[i]-barWeight;
             for (int i2 =0 ; i2 < platesOwned.length; i2++) {
-                if (weightHolder[i] > platesOwned[i2]*2) {
+                if (weightHolder[i] >= platesOwned[i2]*2) {
                     plates[i] += "[" + platesOwned[i2] + "]";
                     weightHolder[i] -= platesOwned[i2]*2;
                 }
+            }
+        }
+        
+        for (int i = 0; i < platesOwned.length; i++) {
+            if (bbbHolder >= platesOwned[i]*2) {
+                bbbPlates += "[" + platesOwned[i] + "]";
+                bbbHolder -= platesOwned[i]*2;
             }
         }
     }
@@ -52,8 +68,9 @@ public class Workout {
     public String getWorkout() {
         String workout = "";
         for (int i=0; i < 3; i++) {
-            workout+= reps[i] + " x " + weight[i] + " " + plates[i] + "\n";
+            workout+= reps[i+offSet] + " x " + weight[i] + " " + plates[i] + "\n";
         }
+        workout += "10 x " + bbbWeight + " " + bbbPlates;
         return workout;
     }
 }
